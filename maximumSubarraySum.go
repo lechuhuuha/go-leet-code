01cd2cc7e0241dc073ec57193b340deda58e3439
lchh0412 <lechuhuuha@gmail.com>
2024-11-19 17:03:08 +0700

    update: added maximumSubarraySum

--- maximumSubarraySum.go
+++ maximumSubarraySum.go
@@ -1,5 +1,24 @@
 package main
 
 func maximumSubarraySum(nums []int, k int) int64 {
+	ans := 0
+	l := len(nums)
+	for i := 0; i < l; i++ {
+		subArray := []int{}
 
+		subArray = append(subArray, nums[i])
+		subArray = append(subArray, nums[i+1])
+		subArray = append(subArray, nums[i+2])
+		ans = max(ans, sumSlice(subArray))
+	}
+
+	return int64(ans)
+}
+
+func sumSlice(numbers []int) int {
+	sum := 0
+	for _, num := range numbers {
+		sum += num
+	}
+	return sum
 }
--- maximumSubarraySum_test.go
+++ maximumSubarraySum_test.go
@@ -1,1 +1,52 @@
 package main
+
+import "testing"
+
+func TestMaximumSubarraySum(t *testing.T) {
+	tests := []struct {
+		name     string
+		nums     []int
+		k        int
+		expected int64
+	}{
+		{
+			name:     "example_1",
+			nums:     []int{1, 5, 4, 2, 9, 9, 9},
+			k:        3,
+			expected: 15,
+		},
+		{
+			name:     "example_2",
+			nums:     []int{4, 4, 4},
+			k:        3,
+			expected: 0,
+		},
+		{
+			name:     "empty_array",
+			nums:     []int{},
+			k:        3,
+			expected: 0,
+		},
+		{
+			name:     "k_greater_than_length",
+			nums:     []int{1, 2},
+			k:        3,
+			expected: 0,
+		},
+		{
+			name:     "no_repeated_elements",
+			nums:     []int{1, 2, 3, 4, 5},
+			k:        2,
+			expected: 9,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := maximumSubarraySum(tt.nums, tt.k)
+			if result != tt.expected {
+				t.Errorf("maximumSubarraySum(%v, %d) = %d; want %d", tt.nums, tt.k, result, tt.expected)
+			}
+		})
+	}
+}
